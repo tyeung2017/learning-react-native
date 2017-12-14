05208350f72cc947a4d9747034944efc441c2869
leo <leo42us@gmail.com>
2017-12-14 22:24:41 +0000

    external linking

--- app/screens/Options.js
+++ app/screens/Options.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import { ScrollView, StatusBar, Platform } from 'react-native';
+import { ScrollView, StatusBar, Platform, Linking } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
 import { ListItem, Separator } from '../components/List';
@@ -10,7 +10,8 @@ const ICON_SIZE = 23;
 const ICON_PREFIX = Platform.OS === 'ios' ? 'ios' : 'md';
 
 const Options = ({ navigation }) => {
-  const handleSitePress = () => alert('site pressed');
+  const handleSitePress = () => Linking.openURL('http://fixer.io')
+                                 .catch(() => alert('error occurred'));
   const handleThemePress = () => navigation.navigate('Themes');
 
   return (
