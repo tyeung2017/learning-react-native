88bc60ed52c4105c1905c2eca01402326fb7f349
leo <leo42us@gmail.com>
2017-12-22 20:38:21 +0000

    currency converter basics complete

--- app/config/saga.js
+++ app/config/saga.js
@@ -2,13 +2,29 @@
 // 1. swao currency
 // 2. change base currency
 // 3. first loaded
-import { takeEvery } from 'redux-saga/effects';
+import { takeEvery, select, call, put } from 'redux-saga/effects';
 
-import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION } from '../actions/currencies';
+import { SWAP_CURRENCY, CHANGE_BASE_CURRENCY, GET_INITIAL_CONVERSION, CONVERSION_ERROR, CONVERSION_RESULT } from '../actions/currencies';
+
+const getRates = currency => fetch(`http://api.fixer.io/latest?base=${currency}`);
 
 function* fetchRates(action) {
-  console.log('action listened by Saga', action);
-  yield;
+  try {
+    let currency = action.currency;
+    if (!currency) {
+      currency = yield select(state => state.currencies.baseCurrency);
+    }
+    const response = yield call(getRates, currency);
+    const result = yield response.json();
+
+    if (result.error) {
+      yield put({ type: CONVERSION_ERROR, error: result.error });
+    } else {
+      yield put({ type: CONVERSION_RESULT, result });
+    }
+  } catch (error) {
+    yield put({ type: CONVERSION_ERROR, error: error.message });
+  }
 }
 
 export default function* rootSaga() {
--- app/screens/Home.js
+++ app/screens/Home.js
@@ -4,6 +4,7 @@ import { StatusBar, KeyboardAvoidingView } from 'react-native';
 import { connect } from 'react-redux';
 
 import { Container } from '../components/Container';
+import { connectAlert } from '../components/Alert';
 import { Logo } from '../components/Logo';
 import { InputWithButton } from '../components/TextInput';
 import { ClearButton } from '../components/Buttons';
@@ -23,10 +24,18 @@ class Home extends Component {
     isFetching: PropTypes.bool,
     lastConvertedDate: PropTypes.object,
     primaryColor: PropTypes.string,
+    alertWithType: PropTypes.func,
+    currencyError: PropTypes.string,
   }
 
   componentWillMount() {
     this.props.dispatch(getInitialConversion());
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (nextProps.currencyError && nextProps.currencyError !== this.props.currencyError) {
+      this.props.alertWithType('error', 'Error', nextProps.currencyError);
+    }
   }
 
   handlePressBaseCurrency = () => this.props.navigation.navigate('CurrencyList', { title: 'Base Currency', type: 'base' });
@@ -83,7 +92,7 @@ class Home extends Component {
 
 const mapStateToProps = ({
   currencies: {
-    baseCurrency, quoteCurrency, amount, conversions,
+    baseCurrency, quoteCurrency, amount, conversions, error,
   },
   theme: { primaryColor },
 }) => {
@@ -100,7 +109,8 @@ const mapStateToProps = ({
     isFetching,
     lastConvertedDate,
     primaryColor,
+    currencyError: error,
   };
 };
 
-export default connect(mapStateToProps)(Home);
+export default connect(mapStateToProps)(connectAlert(Home));
