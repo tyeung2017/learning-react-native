59b271f38f9598511c78afabd3e00d83f320829b
leo <leo42us@gmail.com>
2017-12-25 01:17:41 +0000

    redux persist

--- app/config/store.js
+++ app/config/store.js
@@ -1,4 +1,6 @@
-import { createStore, applyMiddleware } from 'redux';
+import { AsyncStorage } from 'react-native';
+import { createStore, applyMiddleware, compose } from 'redux';
+import { persistStore, autoRehydrate } from 'redux-persist';
 import logger from 'redux-logger';
 import createSagaMiddleware from 'redux-saga';
 
@@ -10,8 +12,16 @@ const middleware = [sagaMiddleware];
 
 if (process.env.NODE_ENV === 'development') { middleware.push(logger); }
 
-const store = createStore(reducers, applyMiddleware(...middleware));
+const store = createStore(
+  reducers,
+  compose(applyMiddleware(...middleware), autoRehydrate())
+);
 
 sagaMiddleware.run(rootSaga);
+
+persistStore(store, {
+  storage: AsyncStorage,
+  blacklist: ['network'],
+});
 
 export default store;
